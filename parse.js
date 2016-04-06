6b62ae555b5c4127ecabf2db94a9dc8bbbf6f949
slofurno <slofurno@gmail.com>
2016-04-06 19:16:14 -0400

    appease smartos

--- parse.js
+++ parse.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 var text = fs.readFileSync("./republic.txt", "utf8");
 var ps = text.split(/\r\n\r\n/g)
   .map(x => x.match(/\S+/g))
-  .filter(x => x && x.length > 150 && x.length < 170)
+  .filter(x => x )
   .map(x => x.join(" "))
-  .slice(-20);
+  .filter(x => x.length > 3000 && x.length < 10000);
 
 ps.forEach(x => console.log(x));
